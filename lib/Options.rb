3aab662775eba52a89cc553e679edced8e57093c
thoran <mail@thoran.com>
2009-12-22 23:51:08 +1100

    removed the OptionParser instance method specific methods on Options with a 'smarter' #method_missing

--- lib/Options.rb
+++ lib/Options.rb
@@ -1,7 +1,7 @@
 # Options
 
 # 20090207
-# 0.4.1
+# 0.4.2
 
 # Description: This provides for a nice wrapper to OptionParser to also act as a store for options provided
 
@@ -9,6 +9,8 @@
 # 1. I've removed the switch list from set and changed it to be a list of switches which defines multiple methods, one per switch.  Much nicer!  
 # 0/1
 # 2. Refactored #on_args, and while it is shorter, I'm doubtful that it is clearer as to what is happening.  I think the longer one may have been clearer?...  
+# 1/2
+# 3. Removed the OptionParser instance method specific methods on Options with a 'smarter' #method_missing.  I'm not sure that this is wise, since in a way I've lost some control over the interface and polluted the 'namespace' for potential switch names.  I'll reconsider this one...  
 
 # Todo:
 # 1. Clean up #set.  
@@ -58,23 +60,15 @@ class Options
     end
   end
   
-  def on(*args, &block)
-    @op.on(*args, &block)
-  end
-  
-  def banner=(s)
-    @op.banner = s
-  end
-  
-  def parse!
-    @op.parse!
-  end
+  private
   
   def method_missing(method_name, *args, &block)
-    @options.send(method_name.to_s, *args, &block)
+    if (@op.methods - Object.instance_methods).include?(method_name.to_s)
+      @op.send(method_name.to_s, *args, &block)
+    else
+      @options.send(method_name.to_s, *args, &block)
+    end
   end
-  
-  private
   
   def on_args(*attrs)
     on_args = []
