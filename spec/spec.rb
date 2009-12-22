d4ca60835c7b55a06f15b47302554b707d352e4f
thoran <mail@thoran.com>
2009-12-22 23:19:00 +1100

    ~ spec/spec.rb

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,11 +1,12 @@
-#20090204, 05
+#20090205
+
 require 'pp'
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/2')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/3')
 
 describe Options, 'when using no block with the constructor' do
   
-  describe 'when using #set' do
+  describe 'and when using #set' do
     setup do
       @options = Options.new
       @options.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
@@ -16,7 +17,7 @@ describe Options, 'when using no block with the constructor' do
     end
   end
   
-  describe 'when using #on' do
+  describe 'and when using #on' do
     setup do
       @options = Options.new
       @options.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
@@ -25,38 +26,42 @@ describe Options, 'when using no block with the constructor' do
     end
     
     it 'should be able to access the value' do
+      @options.form_number
+    end
+  end
+  
+end
+
+describe Options, 'when using a block with the constructor' do
+  
+  describe 'and when using #set' do
     
-    end
-  end
-  
-end
-
-describe Options, 'when using a block with the constructor and #set' do
-  
-  setup do
-    @options = Options.new do |opts|
-      opts.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
-    end
-  end
-  
-  it 'should be able to access the value' do
-    @options.form_name
-  end
-  
-end
-
-describe Options, 'when using a block with the constructor and #on' do
-  
-  setup do
-    @options = Options.new do |opts|
-      opts.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
-        self.form_number = opt
+    setup do
+      @options = Options.new do |opts|
+        opts.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
       end
     end
+    
+    it 'should be able to access the value' do
+      @options.form_name
+    end
+    
   end
   
-  it 'should be able to access the value' do
-    @options.form_number
+  describe 'and when using #on' do
+  
+    setup do
+      @options = Options.new do |opts|
+        opts.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
+          self.form_number = opt
+        end
+      end
+    end
+    
+    it 'should be able to access the value' do
+      @options.form_number
+    end
+    
   end
   
 end
