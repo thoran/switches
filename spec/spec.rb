5dfce725a1e344df23869a98e1c978c80dccc732
thoran <mail@thoran.com>
2009-12-22 23:12:30 +1100

    ~ spec/spec.rb

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,17 +1,27 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/0')
+#20090204, 05
 
-describe 'simple questions' do
+require File.expand_path(File.dirname(__FILE__) + '/../lib/1')
+
+describe Options, "when using no block with the constructor" do
   
-  it "should create options using no block with the constructor" do
-    options = Options.new
-    options.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
-    options.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
+  setup do
+    @options = Options.new
+    @options.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
+    @options.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
       self.form_number = opt
     end
   end
   
-  it "should create options using a block with the constructor" do
-    options = Options.new do |opts|
+  it "should be able to access the values" do
+    @options.form_name.should == 'something'
+  end
+  
+end
+
+describe Options, "when using a block with the constructor" do
+  
+  setup do
+    @options = Options.new do |opts|
       opts.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
       opts.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
         self.form_number = opt
@@ -20,7 +30,7 @@ describe 'simple questions' do
   end
   
   it "should be able to access the values" do
-    options.form_name
+    @options.form_name
   end
   
 end
