3eb09ccf308f28d181c1ac8ea0c642a795ed043d
thoran <mail@thoran.com>
2009-12-23 00:10:47 +1100

    added casting to nominated Ruby classes

--- lib/Options.rb
+++ lib/Options.rb
@@ -1,26 +1,29 @@
 # Switches
 
 # 20091222
-# 0.9.1
+# 0.9.2
 
-# Description: This provides for a nice wrapper to OptionParser to also act as a store for options provided.  
+# Description: Switches provides for a nice wrapper to OptionParser to also act as a store for switches supplied.  
 
 # Todo:
 # 1. Clean up #set.  Done as of 0.4.0.  
 
 # Ideas: 
 # 1. Use ! for options with required switches?  Done as of 0.6.0.  (Changed to being for required arguments in 0.9.0 however.)
+# 2. Do away with optional arguments entirely.  Since when does anyone want to specify a non-boolean switch and then supply no arguments anyway?...  
 
 # Notes: 
 # 1. A limitation is the inability to use the switch, "-?", since there is no Ruby method, #?.  
 # 2. An additional limitation is that the Options class cannot have option-methods which are the same as any OptionParser methods since these will be forwarded to the OptionParser instance in the Options class.  
 # 3. In 0.9.0 I finally had a clear demarcation between switch arguments and switches themselves being mandatory or optional.  This is way more complicated since the adding of numerous methods and instance variables to Switches to accommodate this distiction.  
 # 4. Not that happy with apparently using exclaiming methods in the definition block and then stripping the '!' from the method name before being supplied to OpenStruct.  What to do?...  (Moved the '!' to the method(s) on Switches instead as of 0.9.1.)  
+# 5. It mostly makes sense that the Switches interface methods now hold the '!', since it is modifying that switch in place with a required argument, rather than relying upon a default or otherwise set value elsewhere and presumably later than when the switch was set.  
 
 # Dependencies: 
 # 1. Standard Ruby Library
 
 # Changes since: 0.8:
+# (Renamed this project/class from Options to Switches.)
 # 1. /Options/Switches/.  
 # 2. /RequiredOptionMissing/RequiredSwitchMissing/.  
 # 3. /String#boolean_arg?/String#boolean_switch?/.  
@@ -30,13 +33,23 @@
 # 7. + Switches#supplied_switches.  
 # 8. + Switches#set_unused_switches_to_nil.  
 # 9. ~ self-run section to reflect new interface.  
-# 0/1
+# 0/1 (Removed ! from switch setting methods and moved those to the Switches interface methods.)
 # 10. + Switches#do_set.  
 # 11. ~ Switches#set.  
 # 12. + Switches#set!.  
 # 13. + Switches#required!.  
 # 14. - String#required_arg?.  
 # 15. ~ Switches#on_args + requires_argument.  
+# 1/2 (Added casting to nominated Ruby classes.)
+# 16. ~ Switches#on_args + options.  
+# 17. + Array#extract_options!.  
+# 18. + Switches#allowed.  
+# 19. + Switches#allowed!.  
+# 20. + Swithces#mandatory.  
+# 21. + Switches#necessary.  
+# 22. ~ Switches#do_set + ...options.  
+# 23. ~ Switches#on_args + ...options[:cast].  
+# 24. ~ self-run section to do a simple test of casting.  
 
 require 'ostruct'
 require 'optparse'
@@ -53,6 +66,14 @@ class String
   
   def boolean_switch?
     self =~ /\?$/
+  end
+  
+end
+
+class Array
+  
+  def extract_options!
+    last.is_a?(::Hash) ? pop : {}
   end
   
 end
@@ -89,16 +110,16 @@ class Switches
   end
   
   def set(*attrs, &block)
-    requires_argument = false
-    do_set(requires_argument, *attrs, &block)
+    do_set(false, *attrs, &block)
   end
   alias_method :optional, :set
+  alias_method :allowed, :set
   
   def set!(*attrs, &block)
-    requires_argument = true
-    do_set(requires_argument, *attrs, &block)
+    do_set(true, *attrs, &block)
   end
   alias_method :optional!, :set!
+  alias_method :allowed!, :set!
   
   def required(*attrs, &block)
     @required_switches = @required_switches + attrs.collect{|a| a.to_s.delete('!')}
@@ -111,6 +132,8 @@ class Switches
     @required_switches = @required_switches + attrs.collect{|a| a.to_s.delete('!')}
     set!(*attrs, &block)
   end
+  alias_method :mandatory!, :required!
+  alias_method :necessary!, :required!
   
   def parse!
     @op.parse!
@@ -125,8 +148,9 @@ class Switches
   private
   
   def do_set(requires_argument, *attrs, &block)
+    options = attrs.extract_options!
     @all_switches = @all_switches + attrs.collect{|a| a.to_s.delete('!')}
-    @op.on(*on_args(requires_argument, *attrs, &block)) do |o|
+    @op.on(*on_args(requires_argument, options, *attrs, &block)) do |o|
       attrs.each do |attr|
         @settings.send(attr.to_s.delete('!') + '=', o)
       end
@@ -141,7 +165,7 @@ class Switches
     end
   end
   
-  def on_args(requires_argument, *attrs, &block)
+  def on_args(requires_argument, options, *attrs, &block)
     on_args = []
     boolean_switch = true
     attrs.collect{|e| e.to_s}.each do |attr|
@@ -155,6 +179,7 @@ class Switches
     else
       on_args << (on_args.pop + ' [OPTIONAL_ARGUMENT]')
     end
+    on_args << options[:cast] if options[:cast]
     on_args << yield if block
     on_args
   end
@@ -178,10 +203,12 @@ if __FILE__ == $0
     s.set(:f, :file, :filename){'Optionally provide the name of a file to be read in.'}
     s.set!(:h, :host, :hostname){'The hostname switch is optional, but has a mandatory argument if used.'}
     s.required(:a, :app, :application){'Necessarily provide an application name, but an argument is optional.'}
-    s.required!(:b, :bless, :blessing){'Blessings are always necessary, although they will always cause an argument!'}
+    s.required!(:b, :bless, :blessing){'Blessings are always necessary, yet they will always cause an argument!'}
     s.set(:s?, :sec?, :secure?){'Optionally use a secure connection.'}
     s.required(:r?, :req?, :required?){'Required?'}
     s.optional(:d?, :del?, :delete?){'Optionally delete after action?'}
+    s.optional!(:p, :port, :port_number, :cast => Integer){'Otherwise use the default port and cast ye spell and turn it into an integer.'}
+    s.optional!(:t, :temp, :temperature, :cast => Float){'Taken in the temperature as a float.'}
   end
   pp switches.filename
   pp switches.hostname
@@ -190,6 +217,9 @@ if __FILE__ == $0
   pp switches.secure?
   pp switches.required?
   pp switches.delete?
+  pp switches.port
+  pp switches.degrees
+  pp switches.temperature
   puts
   puts switches.help
   puts
