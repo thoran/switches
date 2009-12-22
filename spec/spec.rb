7b00c4f57f971a79f2f7b7aa4075af85a7912338
thoran <mail@thoran.com>
2009-12-22 23:16:22 +1100

    ~ spec/spec.rb

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,36 +1,62 @@
 #20090204, 05
+require 'pp'
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/1')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/2')
 
-describe Options, "when using no block with the constructor" do
+describe Options, 'when using no block with the constructor' do
   
-  setup do
-    @options = Options.new
-    @options.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
-    @options.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
-      self.form_number = opt
+  describe 'when using #set' do
+    setup do
+      @options = Options.new
+      @options.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
+    end
+    
+    it 'should be able to access the value' do
+      @options.form_name.should == 'FORM'
     end
   end
   
-  it "should be able to access the values" do
-    @options.form_name.should == 'something'
+  describe 'when using #on' do
+    setup do
+      @options = Options.new
+      @options.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
+        self.form_number = opt
+      end
+    end
+    
+    it 'should be able to access the value' do
+    
+    end
   end
   
 end
 
-describe Options, "when using a block with the constructor" do
+describe Options, 'when using a block with the constructor and #set' do
   
   setup do
     @options = Options.new do |opts|
       opts.set(:form_name, '-f', '--form', '--form-name', '--form_name <form_name>', 'If left empty, the first form on the page will be used.')
+    end
+  end
+  
+  it 'should be able to access the value' do
+    @options.form_name
+  end
+  
+end
+
+describe Options, 'when using a block with the constructor and #on' do
+  
+  setup do
+    @options = Options.new do |opts|
       opts.on('-n', '--form-number', '--form_number <form_number>', 'This will set the number of the form in order of appearance on the page.  Use zero-based indexing.') do |opt|
         self.form_number = opt
       end
     end
   end
   
-  it "should be able to access the values" do
-    @options.form_name
+  it 'should be able to access the value' do
+    @options.form_number
   end
   
 end
