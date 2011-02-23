8a44ca26d7d368b3e73c479a21d0f5e069c51ccc
thoran <mail@thoran.com>
2011-02-23 12:04:09 +1100

    1. Added some specs though incomplete; 2. Moved rough testing to examples; 3. Switches 0.9.9 still pending.

--- examples/everything.rb
+++ examples/everything.rb
@@ -1,24 +1,33 @@
 #!/usr/bin/env ruby
 
-# 20100222
+# 20100227, 20100228, 20100301, 20100302
 
 # Changes: 
-# 1. Added #perform and #perform method...  
+# 1. Added #perform and #perform! methods...  
+# 2. + some more stuff to do with #perform.  
+# 3. + #required_perform*.  
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/Switches')
 require 'pp'
 
-"-a --bless me -r --goes bang --go --an_action".split.each{|a| ARGV << a}
+# "-a --bless me --h2 example.com -r -i 5 --goes bang --go --an_action --action2".split.each{|a| ARGV << a}
+"-a --bless me --h2 example.com -r -i 5 --goes bang --go --an_action".split.each{|a| ARGV << a}
 
 def an_action
   pp 'an action'
 end
 
-switches = Switches.new do |s|
+def string
+  'an action'
+end
+
+#switches = Switches.new(:include_casting_interface_methods => true, :to_h => true) do |s|
+switches = Switches.new(:include_casting_interface_methods => true) do |s|
   s.banner = 'Here is a banner.'
   s.set(:z, :zip, :zippy) # Switch summaries are optional.  
   s.set(:f, :file, :filename){'Optionally provide the name of a file to be read in.'}
   s.set!(:h, :host, :hostname){'The hostname switch is optional, but has a mandatory argument if used.'}
+  s.set!(:h2, :host2, :hostname2, :required => true){'The hostname switch is optional, but has a mandatory argument if used.'}
   s.required(:a, :app, :application){'Necessarily provide an application name, but an argument is optional.'}
   s.required!(:bless, :blessing){'Blessings are always necessary, yet they will always cause an argument!'}
   s.set(:s?, :sec?, :secure?){'Optionally use a secure connection.'}
@@ -26,13 +35,29 @@ switches = Switches.new do |s|
   s.optional(:d?, :del?, :delete?){'Optionally delete after action?'}
   s.optional!(:p, :port, :port_number, :cast => Integer){'Otherwise use the default port and cast ye spell and turn it into an integer.'}
   s.optional!(:t, :temp, :temperature, :type => Float){'Temperature will have the type Float.'}
-  s.integer(:i, :int, :default => 31){'Cast me as an int with a default value of 31.'}
+  s.set(:blah, :default => 'blah')
+  s.integer(:i, :int, :default => 5){'Cast me as an int with a default value of 5.'}
+  s.integer(:i2, :int2, :default => '31'){'Cast me as an int with a default value of 31.'}
   s.boolean(:yes_or_no){'This seems like a step backwards by comparison with ?-methods...'}
   s.perform(:go){pp 'go'} # This non-bang method does not require a switch value.  
   s.perform!(:goes){|value| pp value.upcase} # Bang methods do require a switch value, and if not provided the block parameter will be nil and the action will likely fail.  
   s.perform(:an_action){an_action}
+  s.perform(:string){string}
+  s.set(:string2){string}
+  # s.required_perform(:action2){an_action}
 end
 print switches.application == nil ? '.' : 'x'
 print switches.blessing == 'me' ? '.' : 'x'
 print switches.required? == true ? '.' : 'x'
-puts
+print switches.blah == 'blah' ? '.' : 'x'
+
+pp switches.int
+print switches.int == 5 ? '.' : 'x'
+
+# pp switches.int2
+# print switches.int2 == 31 ? '.' : 'x'
+
+# puts switches.help
+# 
+pp switches.to_h
+# pp switches.parse!
---
+++ spec/all.rb
@@ -0,0 +1,222 @@
+require "#{File.dirname(__FILE__)}/../lib/Switches"
+
+class Array
+  
+  alias_method :empty!, :clear
+  
+end
+
+describe Switches do
+  
+  describe ".new" do
+    
+    it "should return an object of class Switches" do
+      Switches.new.class.should == Switches
+    end
+    
+  end
+  
+  describe "#set" do
+    
+    def setup(switches_string = '')
+      ARGV.empty!
+      switches_string.split.each{|a| ARGV << a}
+      Switches.new do |s|
+        s.set :v?, :verbose?
+        s.set :p, :port
+        s.set :host, :h
+      end
+    end
+    
+    it "should set a short boolean switch or flag to true when supplied with a short switch" do
+      setup('-v').v?.should == true
+    end
+    
+    it "should set a long boolean switch value to true when supplied with a short switch" do
+      setup('-v').verbose?.should == true
+    end
+    
+    it "should set a short boolean switch or flag to true when supplied with a long switch" do
+      setup('--verbose').v?.should == true
+    end
+    
+    it "should set a long boolean switch or flag to true when supplied with a long switch" do
+      setup('--verbose').verbose?.should == true
+    end
+    
+    it "should set a short switch value to the value supplied when supplied with a short switch" do
+      setup('-p 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the value supplied when supplied with a short switch" do
+      setup('-p 22').port.should == '22'
+    end
+    
+    it "should set a short switch value to the value supplied when supplied with a long switch" do
+      setup('--port 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the value supplied when supplied with a long switch" do
+      setup('--port 22').port.should == '22'
+    end
+    
+    it "shouldn't matter which order the short and long switches are given" do
+      setup('-h example.com').h.should == 'example.com'
+      setup('--host example.com').h.should == 'example.com'
+    end
+    
+  end # describe "#set"
+  
+  describe "#set!" do
+    
+    def setup(switches_string = '')
+      ARGV.empty!
+      switches_string.split.each{|a| ARGV << a}
+      Switches.new do |s|
+        s.set :p, :port
+      end
+    end
+    
+    it "should set a short switch value to the argument supplied with a short switch" do
+      setup('-p 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the argument supplied with a short switch" do
+      setup('-p 22').port.should == '22'
+    end
+    
+    it "should set a short switch value to the argument supplied with a long switch" do
+      setup('--port 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the argument supplied with a long switch" do
+      setup('--port 22').port.should == '22'
+    end
+    
+    it "should raise a MissingArgument error when no argument is supplied to a short switch" do
+      switches = lambda{setup('-p')}
+      switches.should{raise MissingArgument}
+    end
+    
+    it "should raise a MissingArgument error when no argument is supplied to a long switch" do
+      switches = lambda{setup('--port')}
+      switches.should{raise MissingArgument}
+    end
+    
+  end # describe "#set!"
+  
+  describe "#required" do
+    
+    def setup(switches_string = '')
+      ARGV.empty!
+      switches_string.split.each{|a| ARGV << a}
+      Switches.new do |s|
+        s.required :p, :port
+      end
+    end
+    
+    it "should set a short switch value to the argument supplied with a short switch" do
+      setup('-p 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the argument supplied with a short switch" do
+      setup('-p 22').port.should == '22'
+    end
+    
+    it "should set a short switch value to the argument supplied with a long switch" do
+      setup('--port 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the argument supplied with a long switch" do
+      setup('--port 22').port.should == '22'
+    end
+    
+    it "should raise a RequiredSwitchMissing error no switch is supplied" do
+      switches = lambda{setup}
+      switches.should{raise RequiredSwitchMissing}
+    end
+    
+  end # describe "#required"
+  
+  describe "#required!" do
+    
+    def setup(switches_string = '')
+      ARGV.empty!
+      switches_string.split.each{|a| ARGV << a}
+      Switches.new do |s|
+        s.required! :p, :port
+      end
+    end
+    
+    it "should set a short switch value to the argument supplied with a short switch" do
+      setup('-p 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the argument supplied with a short switch" do
+      setup('-p 22').port.should == '22'
+    end
+    
+    it "should set a short switch value to the argument supplied with a long switch" do
+      setup('--port 22').p.should == '22'
+    end
+    
+    it "should set a long switch value to the argument supplied with a long switch" do
+      setup('--port 22').port.should == '22'
+    end
+    
+    it "should raise a MissingArgument error when no argument is supplied to a short switch" do
+      switches = lambda{setup('-p')}
+      switches.should{raise MissingArgument}
+    end
+    
+    it "should raise a MissingArgument error when no argument is supplied to a long switch" do
+      switches = lambda{setup('-p')}
+      switches.should{raise MissingArgument}
+    end
+    
+    it "should raise a RequiredSwitchMissing error no switch is supplied" do
+      switches = lambda{setup}
+      switches.should{raise RequiredSwitchMissing}
+    end
+    
+  end # describe "#required"
+  
+  describe "#perform" do
+    
+    def setup(switches_string = '')
+      ARGV.empty!
+      switches_string.split.each{|a| ARGV << a}
+      Switches.new do |s|
+        s.perform(:action){an_action}
+      end
+    end
+    
+    def an_action
+      'value'
+    end
+    
+    it "should send(:an_action)"
+    
+  end # describe "#perform"
+  
+  describe "#perform!" do
+    
+    def setup(switches_string = '')
+      ARGV.empty!
+      switches_string.split.each{|a| ARGV << a}
+      Switches.new do |s|
+        s.perform(:action){an_action}
+      end
+    end
+    
+    def an_action
+      'value'
+    end
+    
+    it "should send(:an_action)"
+    
+    it "should require a block parameter"
+    
+  end # describe "#required!"
+  
+end # describe Switches
